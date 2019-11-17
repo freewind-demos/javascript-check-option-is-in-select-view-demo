eff95ddcb7275d15eb79645bc09958bcb09be228
Freewind <nowindlee@gmail.com>
2019-11-17 21:56:46 +0800

    javascript-check-option-is-in-select-view-demo

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-Js Hello World Demo
+JavaScript Check Option is In Select View Demo
 ===================
 
-Open `index.html` in your browser.
+判断一个multiple select中某个option是否可见（选项过多时，滚动到了不可见的位置）
 
-You will see "Hello, JS!" on the page.
+这里的代码判断得不是非常精确（可能有padding等原因），不过基本够用。
+
+Open `index.html` in your browser.
--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-var main = document.getElementById("main");
-main.innerHTML = "Hello, JS!";
+function isInSelectView(option) {
+  const select = option.parentElement;
+  return (select.scrollTop < option.offsetTop)
+    && (select.scrollTop + select.clientHeight > option.offsetTop)
+}
+
+function checkSelectedOptionInSelectView() {
+  const mySelect = document.getElementById('my-select')
+  const selectedOption = mySelect.options[mySelect.selectedIndex]
+  console.log("### selectedOption", selectedOption);
+  console.log("### in view?", isInSelectView(selectedOption));
+}
 
